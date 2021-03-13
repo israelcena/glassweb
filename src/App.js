6c113cbf5cd25e8227163da87e6e4cbc68f38b9b
Israel Cena <israelsjm@gmail.com>
2021-03-13 11:29:27 -0300

    include aside content

--- src/App.js
+++ src/App.js
@@ -4,15 +4,39 @@ import './App.css';
 function App() {
   return (
     <main className='App'>
-     <section className='glass'>
-       <div className='dashboard'>
+      <section className='glass'>
+        <aside className='dashboard'>
           <div className='user'>
-            <img src='./images/avatar.png' alt='avatar' />
+            <img src='./images/avatar.png' alt='avatar'/>
+            <h3>Loren Ipsum</h3>
+            <p>Pro Member</p>
           </div>
-       </div>
-     </section>
-     <div className='circle1'></div>
-     <div className='circle2'></div>
+          <div className="links">
+            <div className="link">
+              <img src="./images/twitch.png" alt="Twitch"/>
+              <h2>Streams</h2>
+            </div>
+            <div className="link">
+              <img src="./images/steam.png" alt="Twitch"/>
+              <h2>Games</h2>
+            </div>
+            <div className="link">
+              <img src="./images/upcoming.png" alt="Twitch"/>
+              <h2>New</h2>
+            </div>
+            <div className="link">
+              <img src="./images/library.png" alt="Twitch"/>
+              <h2>Library</h2>
+            </div>
+          </div>
+          <div className="pro">
+            <h2>Joing pro for free games.</h2>
+            <img src="./images/controller.png" alt="nitendo controler" />
+          </div>
+        </aside>
+      </section>
+      <div className='circle1'></div>
+      <div className='circle2'></div>
     </main>
   );
 }
