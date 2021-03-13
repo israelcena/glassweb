cc720c3aca59b4dad0c8725a56033c9ffb639415
Israel Cena <israelsjm@gmail.com>
2021-03-13 11:38:20 -0300

    include main section

--- src/App.js
+++ src/App.js
@@ -31,9 +31,44 @@ function App() {
           </div>
           <div className="pro">
             <h2>Joing pro for free games.</h2>
-            <img src="./images/controller.png" alt="nitendo controler" />
+            <img src="./images/controller.png" alt="nitendo controler"/>
           </div>
         </aside>
+        <section className="games">
+          <div className="status">
+            <h1>Active Games</h1>
+            <input type="text"/>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <img src="./images/assassins.png" alt="Assassins Cread"/>
+              <div className="card-info">
+                <h2>Assassins Creed Valhalla</h2>
+                <p>PS5 Version</p>
+                <div className="progress"></div>
+              </div>
+              <h2 className="percentage">60%</h2>
+            </div>
+            <div className="card">
+              <img src="./images/sackboy.png" alt="Sack Boy"/>
+              <div className="card-info">
+                <h2>Sack Boy</h2>
+                <p>PC Version</p>
+                <div className="progress"></div>
+              </div>
+              <h2 className="percentage">20%</h2>
+            </div>
+            <div className="card">
+              <img src="./images/spiderman.png" alt="Spider Man"/>
+              <div className="card-info">
+                <h2>Spider Man</h2>
+                <p>PS5 Version</p>
+                <div className="progress"></div>
+              </div>
+              <h2 className="percentage">60%</h2>
+            </div>
+          </div>
+        </section>
       </section>
       <div className='circle1'></div>
       <div className='circle2'></div>
