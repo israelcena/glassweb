42edc13179f33283eba8bd6f40c963a4c9ba4638
Israel Cena <israelsjm@gmail.com>
2021-03-13 10:41:02 -0300

    rework images and add avatar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <main className='App'>
      <section className='glass'>
-       <h1>Hello World!</h1>
+       <div className='dashboard'>
+          <div className='user'>
+            <img src='./images/avatar.png' alt='avatar' />
+          </div>
+       </div>
      </section>
      <div className='circle1'></div>
      <div className='circle2'></div>
