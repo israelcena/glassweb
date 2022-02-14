327080399d9b4f8d4732401ae075e44112adc614
Israel Cena <israelsjm@gmail.com>
2022-02-14 17:01:27 -0300

    create Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React from "react";
+import Dashboard from "./components/Dashboard"
 import "./App.css";
 
 export default function App() {
   return (
     <main className="App">
       <section className="glass">
-        <aside className="dashboard">
-          <div className="user">
-            <img src="./images/avatar.png" alt="avatar" />
-            <h3>Loren Ipsum</h3>
-            <p>Pro Member</p>
-          </div>
-          <div className="links">
-            <div className="link">
-              <img src="./images/twitch.png" alt="Twitch" />
-              <h2>Streams</h2>
-            </div>
-            <div className="link">
-              <img src="./images/steam.png" alt="Twitch" />
-              <h2>Games</h2>
-            </div>
-            <div className="link">
-              <img src="./images/upcoming.png" alt="Twitch" />
-              <h2>New</h2>
-            </div>
-            <div className="link">
-              <img src="./images/library.png" alt="Twitch" />
-              <h2>Library</h2>
-            </div>
-          </div>
-          <div className="pro">
-            <h2>Joing pro for free games.</h2>
-            <img src="./images/controller.png" alt="nitendo controler" />
-          </div>
-        </aside>
+        <Dashboard />
         <section className="games">
           <div className="status">
             <h1>Active Games</h1>
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,33 @@
+export default function Dashboard() {
+  return (
+    <aside className="dashboard">
+      <div className="user">
+        <img src="./images/avatar.png" alt="avatar" />
+        <h3>Loren Ipsum</h3>
+        <p>Pro Member</p>
+      </div>
+      <div className="links">
+        <div className="link">
+          <img src="./images/twitch.png" alt="Twitch" />
+          <h2>Streams</h2>
+        </div>
+        <div className="link">
+          <img src="./images/steam.png" alt="Twitch" />
+          <h2>Games</h2>
+        </div>
+        <div className="link">
+          <img src="./images/upcoming.png" alt="Twitch" />
+          <h2>New</h2>
+        </div>
+        <div className="link">
+          <img src="./images/library.png" alt="Twitch" />
+          <h2>Library</h2>
+        </div>
+      </div>
+      <div className="pro">
+        <h2>Joing pro for free games.</h2>
+        <img src="./images/controller.png" alt="nitendo controler" />
+      </div>
+    </aside>
+  );
+}
