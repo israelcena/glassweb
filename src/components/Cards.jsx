482c0c3f1dddc3c5a19010a0c517a20d7b5f4e3e
Israel Cena <israelsjm@gmail.com>
2022-02-16 21:52:27 -0300

    remove cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,15 @@
 import Card from "./Card";
 
 export default function Cards() {
-  return (
-    <div className="cards">
-            
-   <Card img="./images/assassins.png" alt="Assassins Cread" title="Assassins Creed Valhalla" version="PS5 Version" percentage="60%" />
-
-    <div className="card">
-      <img src="./images/sackboy.png" alt="Sack Boy" />
-      <div className="card-info">
-        <h2>Sack Boy</h2>
-        <p>PC Version</p>
-        <div className="progress"></div>
-      </div>
-      <h2 className="percentage">20%</h2>
-    </div>
-
-    <div className="card">
-      <img src="./images/spiderman.png" alt="Spider Man" />
-      <div className="card-info">
-        <h2>Spider Man</h2>
-        <p>PS5 Version</p>
-        <div className="progress"></div>
-      </div>
-      <h2 className="percentage">60%</h2>
-    </div>
-  </div>
-  )
+	return (
+		<div className="cards">
+			<Card
+				img="./images/assassins.png"
+				alt="Assassins Cread"
+				title="Assassins Creed Valhalla"
+				version="PS5 Version"
+				percentage="60%"
+			/>
+		</div>
+	);
 }
